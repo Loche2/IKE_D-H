75a26d6df0c9951b7cdf70f17bcd5526edacc6a7
LocheZ <1913565532@qq.com>
2023-11-30 01:42:02 +0800

    Completed

--- D-H_client.py
+++ D-H_client.py
@@ -8,21 +8,21 @@ def f(g, x, p):
 
 
 if __name__ == '__main__':
-    privateKey = input("Input your private key:")
+    privateKey = input("输入你的私钥：")
     privateKey = int(privateKey)
     publicKey = f(g, privateKey, p)
 
+    # 连接到服务端
     host = 'localhost'
-    # 启动服务端
     conn = socket(AF_INET, SOCK_STREAM)
     conn.connect((host, 12345))
     print('连接到：', host)
 
-    # 发送公钥给客户端
+    # 发送公钥给服务端
     print(f"发送公钥：{publicKey}")
     conn.send(str(publicKey).encode())
 
-    # 接收客户端消息并打印
+    # 接收服务端消息并打印
     counter_publicKey = conn.recv(1024)
     print(f"对方公钥: {counter_publicKey.decode()}")
 
